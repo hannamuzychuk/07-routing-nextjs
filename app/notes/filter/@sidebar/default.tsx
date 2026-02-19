4e5f0379e20bcc359555319670662363e6305564
Hanna Muzychuk <hannamuzychuk@gmail.com>
2026-02-19 20:02:57 +0100

    make SidebarNotes a server component

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,39 +1,19 @@
-"use client";
-
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import css from "./SidebarNotes.module.css";
 import { NoteTag } from "@/types/note";
 
-const Tags: NoteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
+const tags: NoteTag[] = ["all", "Todo", "Work", "Personal", "Meeting", "Shopping"];
 
-export default function SidebarDefault() {
-  const pathname = usePathname();
-  const segments = pathname?.split("/") || [];
-  const activeTag = segments[segments.length - 1] || "all";
-
+export default function SidebarNotes() {
   return (
-    <aside className={css.sidebar}>
-      <ul className={css.menuList}>
-        <li className={css.menuItem}>
-          <Link
-            href="/notes/filter/all"
-            className={`${css.menuLink} ${activeTag === "all" ? css.active : ""}`}
-          >
-            All notes
+    <ul className={css.menuList}>
+      {tags.map((tag) => (
+        <li key={tag} className={css.menuItem}>
+          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+            {tag === "all" ? "All notes" : tag}
           </Link>
         </li>
-        {Tags.map((tag) => (
-          <li key={tag} className={css.menuItem}>
-            <Link
-              href={`/notes/filter/${tag}`}
-              className={`${css.menuLink} ${activeTag === tag ? css.active : ""}`}
-            >
-              {tag}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </aside>
+      ))}
+    </ul>
   );
 }
